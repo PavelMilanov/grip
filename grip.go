0ae0673582524275098cbea78c6feecf8445211d
pavel.milanov <pawel.milanov@yandex.ru>
2022-11-11 20:46:38 +0300

    рефакторинг

--- grip.go
+++ grip.go
@@ -3,23 +3,12 @@ package main
 import (
 	"log"
 	"os"
-
-	"github.com/joho/godotenv"
 )
 
 func init() {
 	log.SetPrefix("LOG: ")
 	log.SetFlags(log.Ldate | log.Lmicroseconds | log.Llongfile)
 	log.Println("init started")
-}
-
-func env(key string) string {
-	err := godotenv.Load()
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	return os.Getenv(key)
 }
 
 func main() {
