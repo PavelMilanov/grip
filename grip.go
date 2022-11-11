1b3a9687e110a40257238097d4c555a724700ec7
pavel.milanov <pawel.milanov@yandex.ru>
2022-11-11 18:54:25 +0300

    изменен механизм инициализации токена

--- grip.go
+++ grip.go
@@ -4,9 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/PavelMilanov/grip/vscale"
+	"github.com/joho/godotenv"
 )
+
+func env(key string) string {
+	err := godotenv.Load()
+
+	if err != nil {
+		panic("Error loading .env file")
+	}
+	return os.Getenv(key)
+}
 
 func main() {
 
@@ -14,16 +25,30 @@ func main() {
 	vendorProvider := initCommand.String("provider", "vscale", "vendor provider")
 	vendorToken := initCommand.String("token", "", "vendor token")
 
-	if os.Args[1] == "init" {
+	serverCommand := flag.NewFlagSet("server", flag.ExitOnError)
+
+	switch os.Args[1] {
+	case "init":
 		initCommand.Parse(os.Args[2:])
 		switch statusCode := vscale.ValidateAccount(*vendorToken); statusCode {
 		case 200:
-			vendor := fmt.Sprintf("%s_TOKEN", *vendorProvider)
-			os.Setenv(vendor, *vendorToken)
+			file, err := os.OpenFile(".env", os.O_WRONLY|os.O_CREATE, 0666)
+			if err != nil {
+				fmt.Println("Unable to create file:", err)
+				os.Exit(1)
+			}
+			file.WriteString(fmt.Sprintf("%s_TOKEN=%s", strings.ToUpper(*vendorProvider), *vendorToken))
+			defer file.Close()
 			fmt.Println("Token initialized successful!")
 		case 403:
 			fmt.Println("Token invalid!")
 		}
-
+	case "server":
+		serverCommand.Parse(os.Args[2:])
+		token := env("VSCALE_TOKEN")
+		switch os.Args[2] {
+		case "ls":
+			vscale.GetServers(token)
+		}
 	}
 }
