fd882845f06653282d577b73708684094f470385
Pavel Milanov <milanov71@icloud.com>
2023-03-23 14:18:42 +0300

    autogenerate hosts ansible

--- extentions/extentions.go
+++ extentions/extentions.go
@@ -1,11 +1,35 @@
 package extentions
 
 import (
+	"os"
 	"text/template"
 )
 
+type AnsibleHost struct {
+	Vendor string
+	Alias  []AnsibleAlias
+}
 
-host_pipeline := `
+type AnsibleAlias struct {
+	Server, Ip string
+}
+
+func test(model []AnsibleHost) {
+	var hosts = `
+{{range .}}
 [{{.Vendor}}]
+{{range .Alias}}
 {{.Server}} ansible_host={{.Ip}}
+{{end}}
+{{end}}
 `
+
+	tmpl, err := template.New("ansible_hosts").Parse(hosts)
+	if err != nil {
+		panic(err)
+	}
+	err = tmpl.Execute(os.Stdout, model)
+	if err != nil {
+		panic(err)
+	}
+}
