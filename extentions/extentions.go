008fbd171acaf2f894924fe7dbfdc9817cfb3ea1
Pavel Milanov <milanov71@icloud.com>
2023-03-24 16:32:14 +0300

    autogenerate group_vars for ansible

--- extentions/extentions.go
+++ extentions/extentions.go
@@ -15,7 +15,16 @@ type AnsibleAlias struct {
 }
 
 func GenerateAnsibleHostsFile(model []AnsibleHost) {
+	/*
+		Генерирует файл hosts для ansible и генерирует group_vars.
+	*/
 	var hosts = "{{range .}}[{{.Vendor}}]\n{{range .Alias}}{{.Server}} ansible_host={{.Ip}}\n{{end}}{{end}}"
+	exit := make(chan bool)
+	var vendors []string
+	for _, vendor := range model {
+		vendors = append(vendors, vendor.Vendor)
+	}
+	go generateGroupVarsFiles(vendors, exit)
 
 	tmpl, err := template.New("ansible_hosts").Parse(hosts)
 	if err != nil {
@@ -30,4 +39,18 @@ func GenerateAnsibleHostsFile(model []AnsibleHost) {
 		panic(err)
 	}
 	defer file.Close()
+	<-exit
+}
+
+func generateGroupVarsFiles(vendors []string, exit chan bool) {
+	/*
+		Создает файлы по шаблону в директории ansible/group_vars.
+	*/
+	os.Chdir("ansible/group_vars")
+	data := []byte("ansible_user: root\nansible_ssh_private_key_file: ./.ssh/id_rsa")
+	for _, fileName := range vendors {
+		os.Create(fileName)
+		os.WriteFile(fileName, data, 0600)
+	}
+	exit <- true
 }
