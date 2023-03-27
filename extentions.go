79db2a9d987abd0f5484b67b3e01b1bacdb3107e
pavel_milanov <milanov71@icloud.com>
2023-03-27 23:59:43 +0300

    add prototype ansible cli

--- extentions.go
+++ extentions.go
@@ -1,1 +1,19 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func cli_ansible() {
+
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("ansible menu")
+		}
+	}()
+
+	serverCommand := flag.NewFlagSet("ansible", flag.ExitOnError)
+	serverCommand.Parse(os.Args[2:])
+}
